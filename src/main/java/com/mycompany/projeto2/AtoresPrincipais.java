7d4be3a3467fea3e8abe8544afdb5d3d8bb2c634
Joao Martins <jm8244936@gmail.com>
2020-05-14 22:59:40 +0100

    teste atualizado, para a criação dos atores

--- src/main/java/com/mycompany/projeto2/AtoresPrincipais.java
+++ src/main/java/com/mycompany/projeto2/AtoresPrincipais.java
@@ -15,10 +15,6 @@ public class AtoresPrincipais extends Atores
     private ArrayList<Atriz> atrizesPrincipais;
     //Criar um random number que acede ao array dos atores e escolhe um, para o ator principal, o mesmo para a atriz principal
     
-   
-    
-    
-    
     //Criar outro random number que escolhe o numero de atores secundarios e escolhe ao calhas de ambos os arrays, claro que nao pode ser o mesmo que o principal
     
    public AtoresPrincipais(){
