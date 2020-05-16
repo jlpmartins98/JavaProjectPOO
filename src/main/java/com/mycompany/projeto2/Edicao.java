209baa07ca5a240ece2a6dc05ac489681edc8dce
Joao Martins <jm8244936@gmail.com>
2020-05-16 18:48:18 +0100

    teste atualizado caso atores maximo 2

--- src/main/java/com/mycompany/projeto2/Edicao.java
+++ src/main/java/com/mycompany/projeto2/Edicao.java
@@ -74,4 +74,36 @@ public class Edicao {
         }
         return false;
     }
+    
+    
+    
+    
+    //verifica se tem mais de 2 atores nos filmes, em cada edição
+    public boolean maximoAtoresFilmes(Filmes movie, Atores act,Edicao edicao){
+        int contador = 0; 
+
+       
+            for (int i = 0; i < this.filmes.size(); i++) {     //percorre o array dos filmes 
+
+                if(contador <= 2 ){  //enquanto tiver menos de duas instancias de ator 
+                    if(this.filmes.get(i).){             
+                        contador++;                            //incrementa o contador 
+                        movie.addAtores(act);                  //e adiciona o ator a esse filme 
+                    }
+                }
+                else{
+                    System.out.println("O ator só pode estar no máximo em dois filmes diferentes!");
+                    return true;
+                }
+            }
+          
+        return false;
+    }
+    
+    //metodo para mostrar os atores secundarios
+    public void mostraAtores(){
+        for (int i = 0; i < this.filmes.size(); i++) {
+            
+        }
+    }
 }
